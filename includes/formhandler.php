4727d5b56859aee0573f0b1f2424e5c509aa1d3a
jiya <jiyagartaula@gmail.com>
2025-05-04 17:17:34 +0545

    form

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -1,16 +1,25 @@
 <?php
-//var_dump($_SERVER["REQUEST_METHOD"]);
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $firstname =  htmlspecialchars($_POST["firstname"]);
-    $lastname =  htmlspecialchars($_POST["lastname"]);
-    $pets =  htmlspecialchars($_POST["favouritepet"]);
-     
-echo"This are the data ,that the user submitted";
-echo"<br>";
-echo"$firstname";
-echo"<br>";
-echo"$lastname";
-echo"<br>";
-echo"$pets";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = htmlspecialchars($_POST["firstname"]);
+    $lastname = htmlspecialchars($_POST["lastname"]);
+    $pets = htmlspecialchars($_POST["favouritepet"]);
+
+    // Check for empty fields
+    if (empty($firstname) || empty($lastname)) {
+        header("Location: ../form.php");
+        exit(); // exit should come after header, not before
+    }
+
+    // Display submitted data
+    echo "These are the data that the user submitted:<br>";
+    echo "First Name: $firstname<br>";
+    echo "Last Name: $lastname<br>";
+    echo "Favorite Pet: $pets<br>";
+
+    // You usually shouldn't redirect after echoing data
+    // header("Location: ../form.php"); // Remove or move this appropriately
+} else {
+    header("Location: ../form.php");
+    exit();
 }
 ?>
