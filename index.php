576786485da38f0455be3e0e273aeb7afef72a3c
jiya <jiyagartaula@gmail.com>
2025-05-04 19:46:49 +0545

    phpdb

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo"hello";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h3>Signup</h3>
+
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username"><br><br>
+        <input type="password" name="pwd" placeholder="Password"><br><br>
+        <input type="text" name="email" placeholder="E-Mail"><br><br>
+        <button>Signup</button>
+    </form>
+</body>
+</html>
